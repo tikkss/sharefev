5dc5673ac807ca8b7e7aba82a61bcff10e1693d8
tsutomu katsube <tsuto.igawa@gmail.com>
2013-06-01 19:06:09 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharefev::Application.config.secret_token = '7eeb7a543dc673bff1704f2ac8a3738e8bfc9ca4c4185f9a36723c85bdf113b46588c173e2ef0fe36d33909c9cb0a7db1a3f10102a8c4aff143d4d3cb68c4187'
